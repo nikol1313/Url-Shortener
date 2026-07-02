086015fea42fef776f79b420ce0674b0c960114e
peggywhereubeenat <144850283+nikol1313@users.noreply.github.com>
2026-07-02 13:16:15 +0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from app.database import Base
 class URL(Base):
     __tablename__ = "urls"
 
-    id : Mapped[int] = mapped_column(primary_key=True ,index=True)
+    id: Mapped[int] = mapped_column(primary_key=True ,index=True)
     original_url: Mapped[str] = mapped_column(nullable=False, index=True)
     short_url: Mapped[str] = mapped_column(nullable=False, index=True, unique=True)
 
